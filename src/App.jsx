ab6c47c3fb0c174d2e65e4b5470e77bab0a49feb
Carlstorm <carlglainnyuysemo@gmail.com>
2025-03-22 21:41:27 +0100

    arranging the app component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import Login from "./Pages/resgistration/Login";
 import SignUp from "./Pages/resgistration/SignUp";
 import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/home/Home";
-import HeaderNavBar from "./Components/UI/HeaderNavBar";
+
 function App() {
   return (
     <div>
-      {/* <SignUp></SignUp>
-     <Login></Login> */}
-      <HeaderNavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <SignUp></SignUp>
+     <Routes>
+      
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+     </Routes>
+      
     </div>
   );
 }
