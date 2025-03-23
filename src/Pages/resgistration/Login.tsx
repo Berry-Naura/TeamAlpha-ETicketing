c90cefac3b69637a0c5e90032f9d42e6a8526563
Carlstorm <carlglainnyuysemo@gmail.com>
2025-03-23 09:07:54 +0100

    form handling for sign up

--- src/Pages/resgistration/Login.tsx
+++ src/Pages/resgistration/Login.tsx
@@ -2,26 +2,49 @@ import React, { useState } from 'react'
 import "./registration.css"
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import { Link } from'react-router-dom';
+import { Formik,Form,ErrorMessage,Field } from 'formik';
+import * as Yup from 'yup'
 function Login() {
     
-    
-    function handleSubmit(){
+    const [loading, setLoading] =useState(false)
+    function handleSubmit(values){
         console.log("submit clicked");
     }
-        
+    const validationSchema = Yup.object({
+      email: Yup.string()
+       .email('Invalid email')
+       .required('Required'),
+      password: Yup.string()
+       .min(6, 'Password must be at least 6 characters')
+       .required('Required'),
+    }) 
   return (
     <div className='body'>
     <div className='login-container'>
       <h1>Login</h1>
-      <form >
+      <Formik
+      validationSchema={validationSchema}
+      initialValues={{ email: '', password: '' }}
+      onSubmit={handleSubmit}
+      validateOnBlur={true}
+      validateOnChange={true}
+
+      >
+        {({handleSubmit,isValid,dirty})=>(
+      <Form onSubmit={handleSubmit} >
         <label htmlFor="l1">Email</label><br />
-      <input type="text" placeholder='Email' /><br />
-      <label htmlFor="l2">Password</label><br />
-      <input type="password" placeholder= "Type your password" />
+      <Field name="email" type="email" placeholder='Email' />
+      <ErrorMessage name='email' component="div" className='error' />
       <br />
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-      <p className='signup'>Don't have an account? <Link to="#">Sign Up</Link> </p>
-      </form>
+      <label htmlFor="l2">Password</label><br />
+      <Field name="password" type="password" placeholder= "Type your password" />
+      <ErrorMessage name='password' component="div" className='error' />
+      <br />
+      <button type="submit" disabled={!isValid || !dirty || loading} >{loading?"Logging in...":"Submit"}</button>
+      <p className='signup'>Don't have an account? <Link to="/signup">Sign Up</Link> </p>
+      </Form>
+      )}
+      </Formik>
     </div>
     </div>
   )
