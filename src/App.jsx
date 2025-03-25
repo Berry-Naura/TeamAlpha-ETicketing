6f13ecd1a186f70dc5a8ecd1b70bd5a277a0386b
Carlstorm <carlglainnyuysemo@gmail.com>
2025-03-25 20:19:02 +0100

    payment page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Login from "./Pages/resgistration/Login";
 import SignUp from "./Pages/resgistration/SignUp";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/home/Home";
-
+import Payment from "./Pages/payment/Payment";
 function App() {
   return (
     <div>
       {/* <SignUp></SignUp> */}
+      <Payment />
       <Routes>
-      <Route path='/' element={<Home />} />
+      {/* <Route path='/' element={<Home />} /> */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       </Routes>      
